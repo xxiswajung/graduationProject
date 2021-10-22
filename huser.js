8b4e77aa971da609e7fcb9a376f33e985c1dbbb7
ullaice <47681885+ullaice@users.noreply.github.com>
2021-10-22 19:25:31 +0900

    Create huser.js

---
+++ huser.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+class huser extends React.Component{
+    render(){
+        return (
+            <Card>
+                <CardContent>
+                    user
+                </CardContent>
+            </Card>
+            
+        )
+    }
+}
+
+
+export default huser;
